5fac503144442fbb9eaeabffd268a7f5142c13f8
Hemerson Carlin <mersocarlin@gmail.com>
2018-11-05 17:07:42 +0000

    Improve brew.sh

--- brew.sh
+++ brew.sh
@@ -1,9 +1,21 @@
 #!/bin/bash
 
 # Install Homebrew
-/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+command -v brew >/dev/null 2>&1 || { echo >&2 "Installing Homebrew..."; \
+  /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"; }
+
 # macOS Mojave command in case you get an error with Command Line Tools
-# /usr/bin/ruby -e "ARGV=['--force-curl'] $(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+# command -v brew >/dev/null 2>&1 || { echo >&2 "Installing Homebrew..."; \
+#   /usr/bin/ruby -e "ARGV=['--force-curl'] $(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"; }
+
+function install_brew_package() {
+  if brew ls --versions $1 > /dev/null; then
+    echo "$1 is installed"
+  else
+    brew install $1
+  fi
+}
+
 
 # Make sure we’re using the latest Homebrew.
 brew update
@@ -16,60 +28,52 @@ BREW_PREFIX=$(brew --prefix)
 
 # Install GNU core utilities (those that come with macOS are outdated).
 # Don’t forget to add `$(brew --prefix coreutils)/libexec/gnubin` to `$PATH`.
-brew install coreutils
-ln -s "${BREW_PREFIX}/bin/gsha256sum" "${BREW_PREFIX}/bin/sha256sum"
+install_brew_package coreutils
+if [ ! -f "${BREW_PREFIX}/bin/sha256sum" ]; then
+  ln -s "${BREW_PREFIX}/bin/gsha256sum" "${BREW_PREFIX}/bin/sha256sum"
+fi
 
 # Install some other useful utilities like `sponge`.
-brew install moreutils
+install_brew_package moreutils
 
 # Install GNU `find`, `locate`, `updatedb`, and `xargs`, `g`-prefixed.
-brew install findutils
+install_brew_package findutils
 
 # Install GNU `sed`, overwriting the built-in `sed`.
-brew install gnu-sed --with-default-names
-
-# Install Bash 4.
-brew install bash
-brew install bash-completion2
-
-# Switch to using brew-installed bash as default shell
-if ! fgrep -q "${BREW_PREFIX}/bin/bash" /etc/shells; then
-  echo "${BREW_PREFIX}/bin/bash" | sudo tee -a /etc/shells;
-  chsh -s "${BREW_PREFIX}/bin/bash";
-fi;
+install_brew_package gnu-sed --with-default-names
 
 # Install `wget` with IRI support.
-brew install wget --with-iri
+install_brew_package wget --with-iri
 
 # Install more recent versions of some macOS tools.
-brew install vim --with-override-system-vi
-brew install grep
-brew install openssh
-brew install screen
+install_brew_package vim --with-override-system-vi
+install_brew_package grep
+install_brew_package openssh
+install_brew_package screen
 
 # Install font tools.
 brew tap bramstein/webfonttools
-brew install woff2
+install_brew_package woff2
 
 # Install other useful binaries.
-brew install ack
-brew install git
-brew install git-lfs
-brew install imagemagick --with-webp
-brew install jq
-brew install lua
-brew install lynx
-brew install p7zip
-brew install pigz
-brew install pv
-brew install rename
-brew install rlwrap
-brew install ssh-copy-id
-brew install tree
-brew install vbindiff
-brew install yarn --without-node
-brew install zopfli
-brew install zsh
+install_brew_package ack
+install_brew_package git
+install_brew_package git-lfs
+install_brew_package imagemagick --with-webp
+install_brew_package jq
+install_brew_package lua
+install_brew_package lynx
+install_brew_package p7zip
+install_brew_package pigz
+install_brew_package pv
+install_brew_package rename
+install_brew_package rlwrap
+install_brew_package ssh-copy-id
+install_brew_package tree
+install_brew_package vbindiff
+install_brew_package yarn --without-node
+install_brew_package zopfli
+install_brew_package zsh
 
 # Remove outdated versions from the cellar.
 brew cleanup
