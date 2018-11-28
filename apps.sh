c61b1dd934e21373df7ae33f9dc1aa5fca20d1aa
Hemerson Carlin <mersocarlin@gmail.com>
2018-11-28 09:58:04 +0000

    Install nvm as zsh plugin (#10)

--- apps.sh
+++ apps.sh
@@ -1,8 +1,5 @@
 #!/bin/bash
 
-# Install nvm (https://github.com/creationix/nvm)
-wget -qO- https://raw.githubusercontent.com/creationix/nvm/v0.33.11/install.sh | bash
-
 # Install oh-my-zsh (https://github.com/robbyrussell/oh-my-zsh)
 sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 
@@ -13,7 +10,10 @@ chsh -s $(which zsh)
 git clone https://github.com/zsh-users/zsh-autosuggestions $ZSH_CUSTOM/plugins/zsh-autosuggestions
 
 # Install zsh-syntax-highlighting plugin
-git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM}/plugins/zsh-syntax-highlighting
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git $ZSH_CUSTOM/plugins/zsh-syntax-highlighting
+
+# Install zsh-nvm plugin
+git clone https://github.com/lukechilds/zsh-nvm $ZSH_CUSTOM/plugins/zsh-nvm
 
 brew tap caskroom/cask
 brew tap caskroom/versions
